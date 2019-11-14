b81154b2da294b20c58111c172a5b67b4f33978b
jram99 <ramjac13@gmail.com>
2019-11-13 21:12:25 -0500

    Pushing

---
+++ app/src/main/java/com/example/youin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.youin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    var x = 0
+    var y = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
