332cc2566d424dee2bae3c81028a2184f4113f7a
nic2b <nic2bgrube@gmail.com>
2019-11-19 18:07:57 -0500

    Deleted random testing stuff from MainActivity

--- app/src/main/java/com/example/youin/MainActivity.kt
+++ app/src/main/java/com/example/youin/MainActivity.kt
@@ -4,16 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var x = 0
-    var y = 0
-    var test = "Hello"
-    //test
-    //Hello Dybas
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var test2 = "vapegods"
     }
 }
