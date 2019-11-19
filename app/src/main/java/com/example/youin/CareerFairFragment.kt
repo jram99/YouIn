cd44ac57f0e80e5721cd71b665467b11d3ba2d4e
dybasjacob <jacobdybas@gmail.com>
2019-11-18 19:46:35 -0500

    Added a sample career fair fragment and click listeners in MainFeedFragment

--- app/src/main/java/com/example/youin/CareerFairFragment.kt
+++ app/src/main/java/com/example/youin/CareerFairFragment.kt
@@ -7,26 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
-import com.example.youin.databinding.FragmentMainFeedBinding
+import com.example.youin.databinding.FragmentCareerFairBinding
 import com.example.youin.databinding.FragmentSignUpBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class MainFeedFragment : Fragment() {
+class CareerFairFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentMainFeedBinding>(inflater,
-            R.layout.fragment_main_feed,container,false)
-
-        binding.testImage1.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_mainFeedFragment_to_careerFairFragment)
-        }
+        val binding = DataBindingUtil.inflate<FragmentCareerFairBinding>(inflater,
+            R.layout.fragment_career_fair,container,false)
 
         return binding.root
     }
