8a76af678fb80d901bce8705f2dd284c53b4335b
jram99 <ramjac13@gmail.com>
2019-11-21 17:18:00 -0500

    Added nav drawer and About Fragment

---
+++ app/src/main/java/com/example/youin/AboutFragment.kt
@@ -0,0 +1,31 @@
+package com.example.youin
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.youin.databinding.FragmentAboutBinding
+import com.example.youin.databinding.FragmentAboutBindingImpl
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentAboutBinding>(inflater,
+            R.layout.fragment_about,container,false)
+        return binding.root
+
+    }
+
+
+}
--- app/src/main/java/com/example/youin/MainActivity.kt
+++ app/src/main/java/com/example/youin/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.youin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.youin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        this@MainActivity
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        //NavigationUI.setupActionBarWithNavController(this,navController)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 }
