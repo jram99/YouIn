b3a2be12f7a1d7bfb1127d93287c666134eb9289
se-mendheim <50588704+se-mendheim@users.noreply.github.com>
2019-11-21 02:01:03 -0500

    Still some changes to create post

--- app/src/main/java/com/example/youin/CreateNewPost.kt
+++ app/src/main/java/com/example/youin/CreateNewPost.kt
@@ -4,14 +4,13 @@ package com.example.youin
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.DatePicker
 import androidx.databinding.DataBindingUtil
 import com.example.youin.databinding.FragmentCreateNewPostBinding
-import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -21,9 +20,6 @@ import java.util.*
  */
 class CreateNewPost : Fragment() {
 
-    var cal = Calendar.getInstance()
-
-
 
 
     override fun onCreateView(
@@ -31,9 +27,28 @@ class CreateNewPost : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+
         val binding = DataBindingUtil.inflate<FragmentCreateNewPostBinding>(inflater,
             R.layout.fragment_create_new_post,container,false)
 
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        binding.eventDateButton.setOnClickListener {view: View ->
+            Log.println(Log.WARN, "ISSUE!", "I got here")
+            val dpd = DatePickerDialog(view!!.context, DatePickerDialog.OnDateSetListener {
+                    view, year, monthOfYear, dayOfMonth ->
+                binding.eventDate.text = ("$dayOfMonth/$monthOfYear/$year")
+            }, year, month, day)
+
+            dpd.show()
+        }
+
+
+
+/*
         val dateSetListener =
             DatePickerDialog.OnDateSetListener { p0, year, monthOfYear, dayOfMonth ->
                 cal.set(Calendar.YEAR, year)
@@ -43,14 +58,18 @@ class CreateNewPost : Fragment() {
                 val myFormat = "MM/dd/yyyy"
                 val sdf = SimpleDateFormat(myFormat, Locale.US)
                 binding.eventDate.text = sdf.format(cal.getTime())
+
+                print("Yea this works too")
             }
 
         binding.eventDateButton.setOnClickListener {
+
             DatePickerDialog(context!!, dateSetListener,
                 cal.get(Calendar.YEAR),
                 cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH)).show()
-        }
+                print("this totally works")
+        } */
 
 
 
