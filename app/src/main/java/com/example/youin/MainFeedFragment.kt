fee73229e6179f4c419fe55855e1ea7a033b0c36
dybasjacob <jacobdybas@gmail.com>
2019-12-16 23:57:41 -0500

    Test -- More images added to scroll view and calander event started

--- app/src/main/java/com/example/youin/MainFeedFragment.kt
+++ app/src/main/java/com/example/youin/MainFeedFragment.kt
@@ -1,15 +1,20 @@
 package com.example.youin
 
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.youin.databinding.FragmentMainFeedBinding
 import com.example.youin.databinding.FragmentSignUpBinding
+import kotlinx.android.synthetic.main.fragment_main_feed.view.*
+import java.io.File
 
 /**
  * A simple [Fragment] subclass.
@@ -28,9 +33,19 @@ class MainFeedFragment : Fragment() {
             view.findNavController().navigate(R.id.action_mainFeedFragment_to_careerFairFragment)
         }
 
-        binding.floatingActionButton.setOnClickListener {view: View ->
+        binding.testImage2.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_mainFeedFragment_to_homecomingFragment)
+        }
+
+
+        binding.testImage3.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_mainFeedFragment_to_swimFragment)
+        }
+
+        binding.floatingActionButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_mainFeedFragment_to_createNewPost)
         }
+
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/youin/swimFragment.kt
@@ -0,0 +1,25 @@
+package com.example.youin
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class swimFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_swim, container, false)
+    }
+
+
+}
